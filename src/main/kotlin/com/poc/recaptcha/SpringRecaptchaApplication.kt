de15a50541d5c8fa7d816d0bf92e7f6fae0597aa
reidomath <mat.s.ferreira@gmail.com>
2021-06-28 22:07:41 -0300

    first commit

---
+++ src/main/kotlin/com/poc/recaptcha/SpringRecaptchaApplication.kt
@@ -0,0 +1,11 @@
+package com.poc.recaptcha
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringRecaptchaApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringRecaptchaApplication>(*args)
+}
---
+++ src/test/kotlin/com/poc/recaptcha/SpringRecaptchaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.poc.recaptcha
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringRecaptchaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
